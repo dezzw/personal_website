e3e1091603527817093b00dcb9946149003cf31e
dezzw <dw@dezzw.com>
2026-01-25 18:14:51 -0500

    [fix] support all platforms and add missing tailwind typography

--- flake.nix
+++ flake.nix
@@ -3,19 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.bun
-          pkgs.nodejs_20
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bun
+            pkgs.nodejs_20
+          ];
+        };
+      }
+    );
 }
